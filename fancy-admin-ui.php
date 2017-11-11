0adbf1214004422d61ab8c8fa1790ca3e3bb29d3
Will Lopez <will.lopez77@gmail.com>
2017-11-10 22:52:29 -0800

    Disable footer

--- fancy-admin-ui.php
+++ fancy-admin-ui.php
@@ -157,7 +157,7 @@ add_action( 'admin_enqueue_scripts', 'fau_admin_theme_style' );
 function fau_swap_footer_admin() {
   echo 'Custom Admin UI by <a href="http://boborchard.com" target="_blank">Bob Orchard</a> | Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a></p>';
 }
-add_filter( 'admin_footer_text', 'fau_swap_footer_admin' );
+// add_filter( 'admin_footer_text', 'fau_swap_footer_admin' );
 
 // Remove default HTML height on the admin bar callback
 function fui_admin_bar_style() {
